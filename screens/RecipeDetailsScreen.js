595dcf78fa294b48036d0d1ca4d7d75a9858aac0
Diego Gasques <diego.gasques@protonmail.com>
2021-12-04 12:28:49 -0300

    Add favorite button to header

--- screens/RecipeDetailsScreen.js
+++ screens/RecipeDetailsScreen.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import HeaderIconButton from "../components/HeaderIconButton";
+import colors from "../style/theme/colors";
 
 const RecipeDetailsScreen = ({ navigation }) => {
   return (
@@ -11,6 +15,16 @@ const RecipeDetailsScreen = ({ navigation }) => {
 
 RecipeDetailsScreen.navigationOptions = ({ navigation }) => ({
   headerTitle: navigation.getParam("recipe").title,
+  headerRight: () => (
+    <HeaderButtons HeaderButtonComponent={HeaderIconButton}>
+      <Item
+        title="favorite"
+        iconName="ios-star-outline"
+        color={Platform.OS === "android" ? "white" : colors.primary.main}
+        onPress={() => alert("favorite")}
+      />
+    </HeaderButtons>
+  ),
 });
 
 const styles = StyleSheet.create({
